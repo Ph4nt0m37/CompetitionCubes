30a7874fa8286cb6ad20b2c89cb4b829c9042837
Ph4nt0m37 <118580028+Ph4nt0m37@users.noreply.github.com>
2025-09-23 12:09:19 -0400

    fix

--- src/main/java/dev/pakn/competitioncubes/ScrambleController.java
+++ src/main/java/dev/pakn/competitioncubes/ScrambleController.java
@@ -2,12 +2,8 @@ package dev.pakn.competitioncubes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.worldcubeassociation.tnoodle.*;
 import org.worldcubeassociation.tnoodle.scrambles.*;
 
 @Controller
